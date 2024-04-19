a0e58326e5d8653f65afdc1a73db59c0b2bd3004
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-19 06:46:04 +0530

    ⏳ @eshanized updated the repository!!!

--- usr/share/blackbox/scripts/get-the-keys-and-repos.sh
+++ usr/share/blackbox/scripts/get-the-keys-and-repos.sh
@@ -4,7 +4,7 @@
 
 sudo pacman -S wget --noconfirm --needed
 
-echo "Getting the ArcoLinux keys from the ArcoLinux repo - report if link is broken"
+echo "Getting the Snigdha OS keys from the Snigdha PS repo - report if link is broken"
 sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-20251209-3-any.pkg.tar.zst -O /tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
 sudo pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
 
@@ -14,28 +14,20 @@ sudo pacman -U --noconfirm --needed /tmp/arcolinux-mirrorlist-git-23.06-01-any.p
 	
 ######################################################################################################################
 
-if grep -q arcolinux_repo /etc/pacman.conf; then
+if grep -q snigdhaos-core /etc/pacman.conf; then
 
-	echo "ArcoLinux repos are already in /etc/pacman.conf"
+	echo "Snigdha OS repos are already in /etc/pacman.conf"
 
 else
 
 echo '
-#[arcolinux_repo_testing]
-#SigLevel = PackageRequired DatabaseNever
-#Include = /etc/pacman.d/arcolinux-mirrorlist
-
-[arcolinux_repo]
+[snigdhaos-core]
 SigLevel = PackageRequired DatabaseNever
-Include = /etc/pacman.d/arcolinux-mirrorlist
+Include = /etc/pacman.d/snigdhaos-mirrorlist
 
-[arcolinux_repo_3party]
+[snigdhaos-extra]
 SigLevel = PackageRequired DatabaseNever
-Include = /etc/pacman.d/arcolinux-mirrorlist
-
-[arcolinux_repo_xlarge]
-SigLevel = PackageRequired DatabaseNever
-Include = /etc/pacman.d/arcolinux-mirrorlist' | sudo tee --append /etc/pacman.conf
+Include = /etc/pacman.d/snigdhaos-mirrorlist' | sudo tee --append /etc/pacman.conf
 
 fi
 
