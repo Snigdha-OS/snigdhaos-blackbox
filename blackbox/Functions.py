27f2c83a4e7142cf1e28c9e0c4efbfb00864910c
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-23 12:14:02 +0530

    ⏳ @eshanized updated the repository!!!

--- blackbox/Functions.py
+++ blackbox/Functions.py
@@ -1,1 +1,23 @@
 #!/bin/python
+
+import os
+import gi
+
+gi.require_version("Gtk" "3.0") # GTK 2.0 is dead!
+from gi.repository import GLib, Gtk
+
+# NOTE: Base Directory
+base_dir = os.path.dirname(os.path.realpath(__file__))
+
+# NOTE: Global Variables 
+sudo_username = os.getlogin()
+home = "/home/" + str(sudo_username)
+path_dir_cache = base_dir + "/cache/"
+packages = []
+distr = id()
+blackbox_lockfile = "/tmp/blackbox.lock"
+blackbox_pidfile = "/tmp/blackbox.pid"
+process_timeout = 300 # NOTE: process time out has been set to 5 mins.
+snigdhaos_mirrorlist = "/etc/pacman.d/snigdhaos-mirrorlist"
+pacman_conf = "/etc/pacman.conf"
+pacman_conf_backup = "/etc/pacman.conf.bak" # NOTE: Bak stands for backup
