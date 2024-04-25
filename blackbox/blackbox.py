6d7b27eac9057ab2446424e571255b603cad2677
Abhiraj Roy (iconized) <157954129+iconized@users.noreply.github.com>
2024-04-25 16:36:49 +0530

    ui module import()

--- blackbox/blackbox.py
+++ blackbox/blackbox.py
@@ -1,1 +1,15 @@
 #!/bin/python
+
+# UI modules
+from ui.GUI import GUI
+from ui.SplashScreen import SplashScreen
+from ui.ProgressBarWindow import ProgressBarWindow
+from ui.AppFrameGUI import AppFrameGUI
+from ui.AboutDialog import AboutDialog
+from ui.MessageDialog import MessageDialog
+from ui.PacmanLogWindow import PacmanLogWindow
+from ui.PackageListDialog import PackageListDialog
+from ui.ProgressDialog import ProgressDialog
+# from ui.ISOPackagesWindow import ISOPackagesWindow
+from ui.PackageSearchWindow import PackageSearchWindow
+from ui.PackagesImportDialog import PackagesImportDialog
