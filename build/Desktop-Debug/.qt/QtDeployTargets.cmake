c253b57561b7d3cc61cacf970e0d755898c4d12c
eshanized <m.eshanized@gmail.com>
2025-01-09 18:48:01 +0530

    🐛 fix: remove builds

--- build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_snigdhaos-blackbox_FILE /home/whoami/snigdhaos-blackbox/build/Desktop-Debug/snigdhaos-blackbox)
-set(__QT_DEPLOY_TARGET_snigdhaos-blackbox_TYPE EXECUTABLE)
--- build/Desktop-Debug/CMakeFiles/snigdhaos-blackbox.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for snigdhaos-blackbox.
-# This may be replaced when dependencies are built.
